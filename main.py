f99c02445a8a762a09c211cf8b5bdae580aecf18
pesarlankasaitejsa <saitejapesarlanka6@gmail.com>
2024-09-28 20:44:54 +0530

    Updated code

--- main.py
+++ main.py
@@ -25,28 +25,31 @@ scissors = '''
 ---.__(___)
 '''
 
-
 import random
+
 game = [rock, paper, scissors]
 
-a= int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-print(game[a])
+play_again = 'y'
 
-b= random.randint(0, 2)
-print("Computer chose:")
-print(game[b])
+while play_again == 'y':
+    a = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 
-if a>=3 and a<0:
-    print("You typed an invalid number, you lose!")
-elif a==b:
-    print("It's a draw")
-elif a==0 and b==1:
-    print("You loose")
-elif a==1 and b==0:
-  print("You loose")
-elif a==1 and b==2:
-  print("You loose")
-elif a==2 and b==1:
-  print("You loose")
-else:
-    print("You win")
+    if a >= 3 or a < 0:
+        print("You typed an invalid number, you lose!")
+    else:
+        print(game[a])
+
+        b = random.randint(0, 2)
+        print("Computer chose:")
+        print(game[b])
+
+        if a == b:
+            print("It's a draw")
+        elif (a == 0 and b == 2) or (a == 1 and b == 0) or (a == 2 and b == 1):
+            print("You win")
+        else:
+            print("You lose")
+
+    play_again = input("Do you want to play again? Type 'y' for yes or 'n' for no: ").lower()
+
+print("Thanks for playing!")
